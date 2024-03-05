bb15b4004fdc532db5550f75b41e7a7f27304dd1
SriLakshmiSwethaAddepalli <158012350+SriLakshmiSwethaAddepalli@users.noreply.github.com>
2024-03-05 05:58:58 +0530

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -109,7 +109,7 @@ def question3():
     answers["(b) SSE"] = "4*(a**2 + b**2 + R**2)"
 
     # type: a string that evaluates to a float
-    answers["(c) SSE"] = "5*R**2"
+    answers["(c) SSE"] = "10*R**2"
     return answers
 
 
